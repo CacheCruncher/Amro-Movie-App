95406d8d2c7f97b423bd1063230b6b1f88eefc69
CacheCruncher <jawahir.abul@gmail.com>
2026-05-03 10:32:35 +0400

    add Network Result

---
+++ app/src/main/java/com/jawahir/amoro/domain/result/NetworkResult.kt
@@ -0,0 +1,23 @@
+package com.jawahir.amoro.domain.result
+
+/**
+ * Represents every possible outcome of a network operation.
+ *
+ * Use `out T` (Covariance) to allow subtyping. This enables a `NetworkResult<Nothing>`
+ * (like errors) to be treated as a `NetworkResult<T>` for any type `T`.
+ *
+ * ### Usage Example:
+ * ```kotlin
+ * fun getUsername(): NetworkResult<String> {
+ *     return if (isLoggedIn) {
+ *         NetworkResult.Success("Alice") // Returns NetworkResult<String>
+ *     } else {
+ *         NetworkResult.HttpError(401, "Unauthorized") // Returns NetworkResult<Nothing>
+ *     }
+ * }
+ */
+sealed interface NetworkResult<out T>{
+    data class Success<out T>(val data: T) : NetworkResult<T>
+    data class HttpError(val code: Int, val message: String) : NetworkResult<Nothing>
+    data class NetworkError(val throwable: Throwable) : NetworkResult<Nothing>
+}
